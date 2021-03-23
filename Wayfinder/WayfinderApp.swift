7686ab0165e41606df8157bb3bdfe511bc4d992a
boom-roasted <noah.prezant@protonmail.ch>
2021-03-22 22:25:27 -0600

    Initial Commit

---
+++ Wayfinder/WayfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayfinderApp.swift
+//  Wayfinder
+//
+//  Created by lzitney on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WayfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
