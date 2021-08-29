4efa380b042a128a05584b7798ed56ce863f55e9
Noah <noah.prezant@gmail.com>
2021-08-29 17:00:43 -0600

    Add cancel and done buttons to reflection viewe

--- Wayfinder/ContentView.swift
+++ Wayfinder/ContentView.swift
@@ -4,7 +4,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HomeView()
+        NavigationView {
+            VStack {
+                Spacer()
+                NavigationLink(destination: ReflectionView()) {
+                    Text("Reflect")
+                        .font(.system(size: 40))
+                        .padding(EdgeInsets(
+                                    top: 10, leading: 20,
+                                    bottom: 10, trailing: 20))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 40.0)
+                                .stroke(lineWidth: 2.0)
+                    )
+                }
+                Spacer()
+                Text("Summary info")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "square.and.arrow.up")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                            .padding(10)
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ Wayfinder/ReflectionView.swift
@@ -0,0 +1,71 @@
+// Wayfinder
+
+import SwiftUI
+
+struct ReflectionSlider: View {
+    let label: String
+    let binding: Binding<Double>
+    var range: ClosedRange<Double>
+
+    var body: some View {
+        HStack {
+            Text(label)
+            Spacer()
+        }.contentShape(Rectangle()) // Makes entire HStack tappable
+        Slider(
+            value: binding,
+            in: range,
+            step: 1
+        )
+    }
+}
+
+struct ReflectionView: View {
+    @State private var activityName = ""
+    @State private var isFlowState = false
+    @State private var engagementValue = 50.0
+    @State private var energyValue = 0.0
+
+    @State private var isConfirmingCancel = false
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: true) {
+            HStack {
+                TextField("Activity name", text: $activityName)
+                    .font(.system(size: 26))
+                Spacer()
+            }
+            Toggle("Flow state", isOn: $isFlowState)
+            ReflectionSlider(label: "Engagement", binding: $engagementValue, range: 0...100)
+            ReflectionSlider(label: "Energy", binding: $energyValue, range: -100...100)
+            Spacer()
+        }
+        .padding(.horizontal)
+        .navigationBarItems(
+            leading:
+                Button("Cancel") {
+                   isConfirmingCancel = true
+                }
+                .actionSheet(isPresented: $isConfirmingCancel, content: {
+                    ActionSheet(title: Text("Are you sure you want to discard your changes?"),
+                                buttons: [
+                                    .cancel(Text("Keep Editing")),
+                                    .destructive(Text("Discard Changes"), action: { self.presentationMode.wrappedValue.dismiss() })
+                                ]
+                    )
+                }),
+            trailing:
+                Button("Done") {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+        )
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct ReflectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReflectionView()
+    }
+}
