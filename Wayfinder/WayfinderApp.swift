b8c3a3ebe77ea19eb166a6f52333c58320a6ccfe
Noah <noah.prezant@gmail.com>
2021-09-01 07:51:36 -0600

    Add text to tab bar items

--- Wayfinder/WayfinderApp.swift
+++ Wayfinder/WayfinderApp.swift
@@ -9,9 +9,15 @@ struct WayfinderApp: App {
         WindowGroup {
             TabView() {
                 HomeView(dbData: dbData)
-                    .tabItem { Image(systemName: "house.fill") }
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
                 ListView(dbData: dbData)
-                    .tabItem { Image(systemName: "list.bullet") }
+                    .tabItem {
+                        Image(systemName: "list.dash")
+                        Text("Library")
+                    }
             }
             .onAppear {
                 dbData.loadReflections()
