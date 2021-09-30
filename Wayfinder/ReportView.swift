b97374964f31a826eaf1c8bfa7f4d6ba315e9d6a
Noah <noah.prezant@gmail.com>
2021-09-30 13:57:37 -0600

    Add dynamic report pages

--- Wayfinder/ReportView.swift
+++ Wayfinder/ReportView.swift
@@ -2,17 +2,117 @@
 
 import SwiftUI
 
+protocol ReportableView {
+    associatedtype T: View
+    var view: T { get }
+    
+    init(dataStore: DataStore)
+}
+
+enum Report: Int, CaseIterable, Identifiable {
+    case bestOfAll
+    case bestOf
+    case categorical
+    case weekly
+    case daily
+    
+    var id: Int { self.rawValue }
+    
+    var stringValue: String {
+        get {
+            switch self {
+            case .bestOfAll:
+                return "Best of All"
+            case .bestOf:
+                return "Best of"
+            case .categorical:
+                return "Category Average"
+            case .weekly:
+                return "Weekly Average"
+            case .daily:
+                return "Daily Average"
+            }
+        }
+    }
+    
+    func buildView(dataStore: DataStore) -> AnyView {
+        switch self {
+        case .bestOfAll:
+            return AnyView(BestOfAllReportView(dataStore: dataStore))
+        case .bestOf:
+            return AnyView(BestOfReportView(dataStore: dataStore))
+        case .categorical:
+            return AnyView(CategoryReportView(dataStore: dataStore))
+        case .weekly:
+            return AnyView(WeeklyReportView(dataStore: dataStore))
+        case .daily:
+            return AnyView(DailyReportView(dataStore: dataStore))
+        }
+    }
+}
+
 struct ReportView: View {
     @ObservedObject var dataStore: DataStore
+    
+    @State private var selectedIndex: Int = 0
+    @State private var reports: [Report] = [Report.bestOfAll]
+    @State private var addNewPageIsPresented: Bool = false
+    
     var body: some View {
-        TabView {
-            BestOfAllReportView(dataStore: dataStore)
-            BestOfReportView(dataStore: dataStore)
-            CategoryReportView(dataStore: dataStore)
-            WeeklyReportView(dataStore: dataStore)
-            DailyReportView(dataStore: dataStore)
+        VStack {
+            if !reports.isEmpty {
+                TabView(selection: $selectedIndex) {
+                    ForEach(reports.indices, id: \.self) { index in
+                        reports[index].buildView(dataStore: dataStore)
+                            .tag(index)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle())
+            } else {
+                Spacer()
+                Button { addNewPageIsPresented = true } label: {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Create a New Report!")
+                    }
+                }
+                .padding()
+                Spacer()
+            }
+            HStack {
+                Button { removeCurrentPage() } label: { Image(systemName: "trash") }
+                Spacer()
+                Button { addNewPageIsPresented = true } label: { Image(systemName: "plus") }
+            }
+            .padding([.leading, .trailing, .bottom])
         }
-        .tabViewStyle(PageTabViewStyle())
+        .actionSheet(isPresented: $addNewPageIsPresented) {
+            var buttons: [ActionSheet.Button] = []
+            for report in Report.allCases {
+                buttons.append(
+                    .default(Text(report.stringValue.capitalized)) {
+                        let insertionIndex = reports.isEmpty || selectedIndex == reports.count ? reports.endIndex : selectedIndex + 1
+                        withAnimation {
+                            reports.insert(report, at: insertionIndex)
+                            selectedIndex = insertionIndex
+                        }
+                    }
+                )
+            }
+            buttons.append(.cancel())
+            
+            return ActionSheet(title: Text("Choose Report to View"), buttons: buttons)
+        }
+    }
+    
+    func removeCurrentPage() {
+        let indexToRemove = IndexSet(integer: selectedIndex)
+        withAnimation {
+            reports.remove(atOffsets: indexToRemove)
+            if selectedIndex > 0 {
+                selectedIndex -= 1
+            }
+        }
     }
 }
 
