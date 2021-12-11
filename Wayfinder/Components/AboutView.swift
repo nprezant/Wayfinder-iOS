094f0cefce8e39e980e3d66ec5de07b920e27a16
Noah <noah.prezant@gmail.com>
2021-12-11 01:46:07 -0700

    Replace placeholder help text

--- Wayfinder/Components/AboutView.swift
+++ Wayfinder/Components/AboutView.swift
@@ -4,9 +4,37 @@ import SwiftUI
 
 fileprivate let aboutText: String =
 """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Are your passions what you think they are? You may be surprised.
 
-Felis imperdiet proin fermentum leo vel orci porta. Egestas egestas fringilla phasellus faucibus. Sed id semper risus in hendrerit gravida rutrum. Nunc faucibus a pellentesque sit amet porttitor eget. Arcu dictum varius duis at consectetur lorem donec massa sapien. Praesent tristique magna sit amet purus. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Dignissim enim sit amet venenatis. Mi ipsum faucibus vitae aliquet nec ullamcorper. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Fermentum iaculis eu non diam. Ornare arcu odio ut sem.
+Specifically, what makes you engaged? What makes you energized? And what puts you in a flow state?
+
+Inspired by the book "Designing Your Life" by Bill Burnett and Dave Evans, Wayfinder offers a unique mode of journaling: it mimics Burnett and Evans' concept of the "Good Times Journal," which asks you to reflect on your engagement, your energy, and whether you’re in a flow state for any given activity.
+
+The “Good Times Journal” aims to give you data to help you re-design your days…and maybe even your life. You’ll learn what you really like. And what you really dislike. And you can start to organize your day with more intention. Maybe you can sandwich a draining activity between two energizing activities? Could you consider paying your kids a couple dollars to fold laundry because it turns out to be your least favorite thing you do?
+
+Wayfinder provides a number of features that set it apart from the paper and pen version described in the book:
+
+-“Tagging” activities helps you categorize your reflections in new ways. Perhaps you dislike "making pasta," but you really like "cooking," in general. Then you can start to ask questions like “what makes cooking pasta so much worse than inventing that stir fry recipe? How can I change the way I make pasta to mimic what I love about making stir fry?”
+
+-“Reports” help you quickly digest your reflections with enlightening summaries. For instance, one of our favorite reports ranks your tags from those you like most to those you like least. Maybe you’ll learn you like “house cleaning” activities more than you like tasks involving “math” – and you’re considering a PhD in mathematics!
+
+-"Views" within the app allow you to have separate “journals” for different aspects of your life. Maybe want your singing lessons and hikes to be recorded in a “journal” about all of your “play” activities. And maybe you want a “work” journal to record how you feel about spreadsheets and washing dishes. Or maybe you just want one “everything” journal. The choice is yours.
+
+-“Views” are also customizable, so you can rename “work” to “summer job” on the fly, or you can combine your “work” and “babysitting” views without a hassle.
+
+There are many pathways through life. And better yet, there are many “right” pathways through life. We hope Wayfinder will help you find your way forward – toward a more engaging, energizing, fulfilling life.
+
+---
+
+Praise for the Wayfinder app:
+
+“Thank goodness it’s done!” – Noah Prezant | Developer
+
+“Wow! Now I might finally keep a Good Times Journal.” – Lauren Zitney | person who initially requested this app, wrote this description text, and thought about the occasional UX design element
+
+---
+
+Lastly, we think this app may be best used in 2-3 week “sprints.” When you notice a shift in your life or how you’re feeling about your life, consider using Wayfinder to help you gather some real-life data during short re-evaluation periods. And then let us know if you agree! We’d love to learn more about the ways in which you find this tool useful.
 """
 
 struct AboutView: View {
