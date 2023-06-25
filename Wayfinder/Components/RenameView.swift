560ffd9fb20f89583a83f1c031ef30101937e3a1
Noah <noah.prezant@gmail.com>
2023-06-24 19:50:12 -0600

    WIP improving text fields

--- Wayfinder/Components/RenameView.swift
+++ Wayfinder/Components/RenameView.swift
@@ -10,10 +10,24 @@ struct RenameView: View {
     
     @State var newName: String = ""
     
+    private enum Field: Int, Hashable {
+        case name
+    }
+    
+    @FocusState private var focusedField: Field?
+    
     var body: some View {
         VStack {
-            HStack { Text("Rename '\(oldName)' to...").font(.title); Spacer() }.padding()
-            TextField("New name", text: $newName).padding()
+            HStack { Text("Rename View"); Spacer() }.font(.title).padding()
+            HStack { Text("Old Name:"); Text("\(oldName)").underline(); Spacer() }.padding()
+            if #available(iOS 15.0, *) {
+                TextField("Type new name here...", text: $newName)
+                    .focused($focusedField, equals: .name)
+                    .padding()
+            } else {
+                TextField("Type new name here...", text: $newName)
+                    .padding()
+            }
             let nameIsInvalid = invalidNames.contains(newName)
             if nameIsInvalid {
                 Text("A '\(newName)' view already exists")
@@ -25,7 +39,7 @@ struct RenameView: View {
                isPresented = false
             } label: {
                 Text("Rename")
-                    .disabled(nameIsInvalid)
+                    .disabled(nameIsInvalid || newName.isEmpty)
             }.padding()
             Spacer()
         }
@@ -38,3 +52,51 @@ struct RenameView_Previews: PreviewProvider {
         RenameView(isPresented: .constant(true), oldName: "Work", invalidNames: [], saveAction: {_ in})
     }
 }
+
+
+
+
+
+
+
+
+
+struct CustomTextField: UIViewRepresentable {
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+
+        @Binding var text: String
+        var didBecomeFirstResponder = false
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            text = textField.text ?? ""
+        }
+
+    }
+
+    @Binding var text: String
+    var isFirstResponder: Bool = false
+
+    func makeUIView(context: UIViewRepresentableContext<CustomTextField>) -> UITextField {
+        let textField = UITextField(frame: .zero)
+        textField.delegate = context.coordinator
+        return textField
+    }
+
+    func makeCoordinator() -> CustomTextField.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<CustomTextField>) {
+        uiView.text = text
+        if isFirstResponder && !context.coordinator.didBecomeFirstResponder  {
+            uiView.becomeFirstResponder()
+            context.coordinator.didBecomeFirstResponder = true
+        }
+    }
+}
+
